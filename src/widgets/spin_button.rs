fe87338cb8724a4e308c53a40e6f0f50fb678a89
Mia McMahill <84749759+electricbrass@users.noreply.github.com>
2025-07-11 21:53:05 -0500

    Add missing gpl headers

--- src/widgets/spin_button.rs
+++ src/widgets/spin_button.rs
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2025  Mia McMahill
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 // TODO: make a different spin button widget that lets you click the number to type it
 // looks like theres plans to add this functionality to the built in widget
 // so if we match the design to the design docs, we can make a pr to libcosmic if contributors agree to letting this bit be used as MPLv2
